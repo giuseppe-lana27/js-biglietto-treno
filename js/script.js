695b7490adb9492a25e0a264fc03424e91d96238
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-02-26 15:48:43 +0100

    inserito calcolo prezzo biglietto

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ var eta = parseInt(prompt("Inserisci la tua eta'"));
 // 3. Calcolo prezzo biglietto
 var prezzo = 0.21;
 var prezzoTotale = km * prezzo;
+// 4. Calcolo sconto
+var prezzoScontato;
+if (eta < 18){
+  prezzoScontato = ((prezzoTotale * 20) / 100);
+  document.getElementById('prezzo').innerHTML = prezzoScontato + " &#x20AC;";
+} else if (eta >= 65) {
+  prezzoScontato = ((prezzoTotale * 40) / 100);
+  document.getElementById('prezzo').innerHTML = prezzoScontato + " &#x20AC;";
+} else {
+  document.getElementById('prezzo').innerHTML = prezzoTotale + " &#x20AC;"
+}
