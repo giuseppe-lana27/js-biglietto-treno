b56c952413d496777cd7317678bbec945ab1d190
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-02-26 15:43:09 +0100

    inizio progetto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1. Inserimento numero di km da percorrere
+var km = parseInt(prompt("Inserisci numero di Km da percorrere"));
+// 2. Inserimento eta' viaggiatore
+var eta = parseInt(prompt("Inserisci la tua eta'"));
+// 3. Calcolo prezzo biglietto
+var prezzo = 0.21;
+var prezzoTotale = km * prezzo;
